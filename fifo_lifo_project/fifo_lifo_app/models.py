36865a44c33ae6294ae461b2884d441292f5b2e3
dimafdrnk <dimawkfdrng@gmail.com>
2022-07-30 13:33:11 +0300

    Задание 21. Переход на хранение данных в SQLight.

--- fifo_lifo_project/fifo_lifo_app/models.py
+++ fifo_lifo_project/fifo_lifo_app/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
 
-class Stock(models.Model):
-    address = models.CharField(db_column='Адрес', max_length=100)
+# class Stock(models.Model):
+#     address = models.CharField(db_column='Адрес', max_length=100)
 
 
 class Donation(models.Model):
-    stocks = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    item = models.CharField(db_column='Название', max_length=30)
-    amount = models.IntegerField(db_column='Количество')
+    # stocks = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    amount = models.IntegerField()
 
 
-class Donate(models.Model):
-    donation = models.ForeignKey(Donation, on_delete=models.CASCADE)
-    amount = models.IntegerField(db_column='Количество')
-    issuing_time = models.DateTimeField(db_column='Дата выдачи')
-    full_name_recipients = models.CharField(db_column='ФИО', max_length=50)
+
+# class Donate(models.Model):
+#     donation = models.ForeignKey(Donation, on_delete=models.CASCADE)
+#     amount = models.IntegerField(db_column='Количество')
+#     issuing_time = models.DateTimeField(db_column='Дата выдачи')
+#     full_name_recipients = models.CharField(db_column='ФИО', max_length=50)
--- fifo_lifo_project/fifo_lifo_app/views.py
+++ fifo_lifo_project/fifo_lifo_app/views.py
@@ -1,17 +1,40 @@
+from django.db.models import F
 from django.shortcuts import render
 
-from fifo_lifo import Stock
+from .models import Donation
 
-stock_1 = Stock("fifo")
 
 def index(request):
     return render(request, 'fifo_lifo_templates/home_page.html')
 
+
 def donation(request):
-    return render(request, 'fifo_lifo_templates/donation_page.html', {'stock': stock_1.gift()})
+    method = "fifo"
+    if method == "fifo":
+        donation_item = Donation.objects.order_by('-id')[:1]
+    elif method == "lifo":
+        donation_item = Donation.objects.order_by('id')[:1]
+
+    for item in donation_item:
+        if item.amount > 1:
+            Donation.objects.filter(name=item.name).update(amount=item.amount - 1)
+        else:
+            Donation.objects.filter(name=item.name).delete()
+
+    context = {
+        'donation_item': donation_item
+    }
+    return render(request, 'fifo_lifo_templates/donation_page.html', context)
+
 
 def donate(request):
-    name = request.POST["name"]
-    amount = request.POST["amount"]
-    stock_1.donation(name, int(amount))
-    return render(request, 'fifo_lifo_templates/donate_page.html')
+    donate_item = Donation.objects.filter(name=request.POST["name"])
+    if donate_item:
+        Donation.objects.filter(name=request.POST["name"]).update(amount=F('amount') + request.POST["amount"])
+    else:
+        Donation.objects.create(name=request.POST["name"], amount=request.POST["amount"])
+
+    context = {
+        'donate_item': donate_item
+    }
+    return render(request, 'fifo_lifo_templates/donate_page.html', context)
