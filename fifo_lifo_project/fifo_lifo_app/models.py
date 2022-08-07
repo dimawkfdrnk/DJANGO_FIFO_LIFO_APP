29e666d034e38dcd402eef4ea2e220ada7678d61
dimafdrnk <dimawkfdrng@gmail.com>
2022-08-07 16:10:19 +0300

    FIFO_LIFO_PROJECT

--- fifo_lifo_project/fifo_lifo_app/models.py
+++ fifo_lifo_project/fifo_lifo_app/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 
 
-# class Stock(models.Model):
-#     address = models.CharField(db_column='Адрес', max_length=100)
+class Stocks(models.Model):
+    name_stock = models.CharField(max_length=100)
+
 
 
 class Donation(models.Model):
-    # stocks = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stocks, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     amount = models.IntegerField()
+    state = models.CharField(max_length=20)
 
 
 
-# class Donate(models.Model):
-#     donation = models.ForeignKey(Donation, on_delete=models.CASCADE)
-#     amount = models.IntegerField(db_column='Количество')
-#     issuing_time = models.DateTimeField(db_column='Дата выдачи')
-#     full_name_recipients = models.CharField(db_column='ФИО', max_length=50)
+class Donate(models.Model):
+    donation = models.ForeignKey(Donation, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    issuing_time = models.DateTimeField()
+    full_name_recipients = models.CharField(max_length=50)
--- fifo_lifo_project/fifo_lifo_app/views.py
+++ fifo_lifo_project/fifo_lifo_app/views.py
@@ -1,25 +1,22 @@
-from django.db.models import F
 from django.shortcuts import render
 
 from .models import Donation
 
+# from .forms import DonationForm
 
 def index(request):
     return render(request, 'fifo_lifo_templates/home_page.html')
 
 
 def donation(request):
-    method = "fifo"
-    if method == "fifo":
-        donation_item = Donation.objects.order_by('-id')[:1]
-    elif method == "lifo":
-        donation_item = Donation.objects.order_by('id')[:1]
-
-    for item in donation_item:
-        if item.amount > 1:
-            Donation.objects.filter(name=item.name).update(amount=item.amount - 1)
-        else:
-            Donation.objects.filter(name=item.name).delete()
+    donation_item = Donation.objects.exclude(state="booked")
+    method = "lifo"
+    if method == "fifo" and donation_item:
+        donation_item = donation_item.latest("id")
+        Donation.objects.filter(id=donation_item.id).update(state="booked")
+    elif method == "lifo" and donation_item:
+        donation_item = donation_item.first()
+        Donation.objects.filter(id=donation_item.id).update(state="booked")
 
     context = {
         'donation_item': donation_item
@@ -28,13 +25,10 @@ def donation(request):
 
 
 def donate(request):
-    donate_item = Donation.objects.filter(name=request.POST["name"])
-    if donate_item:
-        Donation.objects.filter(name=request.POST["name"]).update(amount=F('amount') + request.POST["amount"])
-    else:
-        Donation.objects.create(name=request.POST["name"], amount=request.POST["amount"])
-
-    context = {
-        'donate_item': donate_item
-    }
-    return render(request, 'fifo_lifo_templates/donate_page.html', context)
+    Donation.objects.create(
+        name=request.POST["name"],
+        amount=request.POST["amount"],
+        stock_id=9,
+        state="available"
+    )
+    return render(request, 'fifo_lifo_templates/donate_page.html')
