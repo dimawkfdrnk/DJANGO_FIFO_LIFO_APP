1e79e5889e93a4641566ca0164261848b14d0ec4
dimafdrnk <dimawkfdrng@gmail.com>
2022-09-06 14:45:55 +0300

    unit 24, use of proxy models and models manager

--- fifo_lifo_project/fifo_lifo_app/models.py
+++ fifo_lifo_project/fifo_lifo_app/models.py
@@ -4,45 +4,73 @@ from django.dispatch import receiver
 from django.db.models import Sum, Count, F
 from django.db import transaction
 
-class AbstractModel_1(models.Model):
-    full_name = models.CharField(max_length=30)
+
+# class Stocks(models.Model):
+#     name_stock = models.CharField(max_length=100)
+#     vacancies = models.IntegerField(null=True, default=0)
+#     occupied_places = models.IntegerField(null=True, default=0)
+#
+#     class Meta:
+#         constraints = [
+#             models.CheckConstraint(check=models.Q(vacancies__gte=F('occupied_places')), name="occupied_places_gte")
+#         ]
+#
+#     def __str__(self):
+#         return self.name_stock
+
+
+
+class HelpRequest(models.Model):
+    full_name_petitioner = models.CharField(max_length=50)
+    status = models.CharField(max_length=15, choices= (('Open', 'Open'), ('Close', 'Close')), default='Open')
+    # stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE, null=True)
+
+
+class Donation(models.Model):
+    full_name_donator = models.CharField(max_length=50)
+    # stock_id = models.ForeignKey(Stocks, on_delete=models.CASCADE, null=True)
+
+
+class ItemDescription(models.Model):
+    name_item = models.CharField(max_length=50)
+    # amount = models.IntegerField(null=True, default=0)
+    # status = models.CharField(max_length=30)
 
     class Meta:
         abstract = True
 
 
-class AbstractModel_2(models.Model):
-    name_thing = models.CharField(max_length=40)
+class DonationItem(ItemDescription):
+    donation = models.ForeignKey(Donation, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=15, choices=(('Free', 'Free'), ('Given_away', 'Given_away')), default='Free')
+
+
+class RequestItem(ItemDescription):
+    request = models.ForeignKey(HelpRequest, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=15, choices=(('Expects', 'Expects'), ('Received', 'Received')), default='Free')
+
+
+
+class ManagerHelpRequest(models.Manager):
+    def get_close_request(self):
+        return super().get_queryset().filter(status='Close')
+
+
+class CompletedRequest(HelpRequest):
 
     class Meta:
-        abstract = True
+        proxy = True
 
-
-class HelpRequest(AbstractModel_1):
-    pass
-
-
-
-class RequestThings(AbstractModel_2):
-    number_help = models.ForeignKey(HelpRequest, on_delete=models.CASCADE, null=True)
-
-
-
-class Donation(AbstractModel_1):
-    pass
-
-
-
-class DonationThings(AbstractModel_2):
-    number_donation = models.ForeignKey(Donation, on_delete=models.CASCADE, null=True)
+    object = ManagerHelpRequest()
 
 
 
 
 
-
-
-
+# @receiver(post_save, sender=Donation)
+# def func(sender, instance, **kwargs):
+#     c = sender.objects.filter(stock_id=instance.stock_id, state="available")
+#     Stocks.objects.select_for_update().filter(id=instance.stock_id).update(occupied_places=c.aggregate(Sum('amount'))['amount__sum'])
 
 
 
--- fifo_lifo_project/fifo_lifo_app/views.py
+++ fifo_lifo_project/fifo_lifo_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.db.models import F
 # from .forms import DonationForm
-from .models import RequestThings,HelpRequest,DonationThings,Donation
+from .models import DonationItem,HelpRequest,RequestItem,Donation
 from django.db import transaction
 
 
@@ -19,10 +19,9 @@ def help_request(request):
 def request_things(request):
     if request.method == "POST":
         data = request.POST
-        number_request = HelpRequest.objects.create(full_name=request.POST['full_name_petitioner'])
-        print(number_request)
+        request_id = HelpRequest.objects.create(full_name_petitioner=request.POST['full_name_petitioner'])
         for number in range(int(data['amount_thing'])):
-            RequestThings.objects.create(name_thing=data[f"name{number}"], number_help=number_request)
+            RequestItem.objects.create(name_item=data[f"name{number}"], request_id=request_id)
     return render(request, 'fifo_lifo_templates/home_page.html')
 
 
@@ -36,9 +35,9 @@ def donation(request):
 def donation_things(request):
     if request.method == "POST":
         data = request.POST
-        number_donation = Donation.objects.create(full_name=request.POST['full_name_donator'])
+        donation_id = Donation.objects.create(full_name_donator=request.POST['full_name_donator'])
         for number in range(int(data['amount_thing'])):
-            DonationThings.objects.create(name_thing=data[f"name{number}"], number_donation=number_donation)
+            DonationItem.objects.create(name_item=data[f"name{number}"], donation_id=donation_id)
     return render(request, 'fifo_lifo_templates/home_page.html')
 
 
@@ -47,6 +46,44 @@ def donation_things(request):
 
 
 
-
+# def index(request):
+#     if request.session.has_key('donate'):
+#         session_data = request.session['donate']
+#         form = DonationForm(initial={
+#             'stock': session_data['stock_id'],
+#             'full_name_donator': session_data['full_name'],
+#         })
+#     else:
+#         form = DonationForm()
+#
+#     return render(request, 'fifo_lifo_templates/home_page.html', {"form": form})
+#
+# @transaction.atomic()
+# def donation(request):
+#     donation_item = Donation.objects.select_for_update().exclude(state="booked")
+#     method = "lifo"
+#     if method == "fifo" and donation_item:
+#         donation_item = donation_item.latest("id")
+#         donation_item.state = "booked"
+#         donation_item.save()
+#
+#     elif method == "lifo" and donation_item:
+#         donation_item = donation_item.first()
+#         donation_item.state = "booked"
+#         donation_item.save()
+#     return render(request, 'fifo_lifo_templates/help_request.html', {'donation_item': donation_item})
+#
+# @transaction.atomic()
+# def donate(request):
+#     if request.method == "POST":
+#         form = DonationForm(request.POST)
+#         if form.is_valid():
+#             data_for_session = Donation.objects.create(**form.cleaned_data)
+#             request.session['donate'] = {
+#                 "stock_id": data_for_session.stock.id,
+#                 "full_name": data_for_session.full_name_donator
+#             }
+#
+#     return render(request, 'fifo_lifo_templates/donation.html')
 
 
