67200b4ea59e3235af54742e5b382262d96f03bf
dimafdrnk <dimawkfdrng@gmail.com>
2022-09-13 15:40:08 +0300

    Unit 24. Disabled button, oneChange

--- fifo_lifo_project/fifo_lifo_app/views.py
+++ fifo_lifo_project/fifo_lifo_app/views.py
@@ -1,5 +1,6 @@
 from django.db import transaction
 from django.shortcuts import render
+
 from .models import DonationItem, HelpRequest, RequestItem, Donation, Stocks
 
 
@@ -10,19 +11,25 @@ def index(request):
 def help_request(request):
     if request.method == "POST":
         amount_items = int(request.POST['amount_items'])
-    return render(request, 'fifo_lifo_templates/request_item.html',
-                  {"amount_items": amount_items, 'stock': Stocks.objects.all()})
+    context = {
+        "amount_items": amount_items,
+        'stocks': Stocks.objects.all()
+    }
+
+    return render(request, 'fifo_lifo_templates/request_item.html', context)
 
 
 @transaction.atomic()
 def request_item(request):
+
     context = {
         'answer': {}
-
     }
+
     if request.method == "POST":
         data = request.POST
         request_object = HelpRequest.objects.create()
+
         for number in range(int(data['amount_items'])):
             request_item = RequestItem.objects.create(
                 name_item=data[f"name{number}"],
@@ -32,12 +39,14 @@ def request_item(request):
                 name_item=request_item.name_item,
                 status='Free',
                 stock_id=request.POST['id_stock']).last()
-            context['answer'][request_item.name_item] = donation_item
+
             if donation_item:
                 request_item.status = 'Close'
                 donation_item.status = 'Issued'
                 request_item.save()
                 donation_item.save()
+                context['answer'][request_item.name_item] = donation_item
+
         help_request_check = RequestItem.objects.filter(request_id=request_object.id, status='Open')
         if not help_request_check:
             request_object.status = 'Close'
@@ -53,20 +62,37 @@ def end_registration(request):
 def donation(request):
     if request.method == "POST":
         amount_items = int(request.POST['amount_items'])
-    return render(request, 'fifo_lifo_templates/donation_item.html', {
+        id_stock = None
+        disabled_button  = False
+        if 'id_stock' in request.POST:
+            id_stock = request.POST['id_stock']
+        stock = Stocks.objects.filter(id=id_stock)
+        for i in stock:
+            if i.occupied_places >= i.vacancies:
+                disabled_button = True
+
+    context = {
         "amount_items": amount_items,
-        'stock': Stocks.objects.all()})
+        'stocks': Stocks.objects.all(),
+        'id_stock': id_stock,
+        'disabled_button': disabled_button
+    }
+
+    return render(request, 'fifo_lifo_templates/donation_item.html', context)
 
 
 @transaction.atomic()
 def donation_item(request):
     if request.method == "POST":
         data = request.POST
+        # print(data)
         donation_object = Donation.objects.create()
+
         for number in range(int(data['amount_items'])):
             DonationItem.objects.create(
                 name_item=data[f"name{number}"],
                 donation_id=donation_object.id,
                 stock_id=request.POST['id_stock']
             )
+
     return render(request, 'fifo_lifo_templates/home_page.html')
